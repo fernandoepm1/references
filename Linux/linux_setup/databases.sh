87b50a062531552c38dddea01fad8f91ec050aca
Fernando <fernando.epm1@gmail.com>
2020-07-21 18:59:36 -0300

    Update pgadmin4 installation

--- Linux/linux_setup/databases.sh
+++ Linux/linux_setup/databases.sh
@@ -23,9 +23,11 @@ echo Installing pgadmin4
 echo -------------------
 echo
 
-# Add key and repo
+# Add GPG key
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc \
 	| sudo apt-key add -
+
+# Add pgadmin repo
 echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" \
 	| sudo tee  /etc/apt/sources.list.d/pgdg.list
 
