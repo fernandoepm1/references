e07c97819c93b662883120c2a729b04ea0b12f53
Fernando <fernando.epm1@gmail.com>
2020-07-21 18:45:25 -0300

    Separate software installation in it's own file

---
+++ Linux/software.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+echo -----------------
+echo Installing chrome
+echo -----------------
+echo
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo rm google-chrome-stable_current_amd64.deb
+
+echo -----------------
+echo Installing vscode
+echo -----------------
+echo
+
+# Dependencies
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+
+# Importing Microsoft GPG key
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- \
+  | sudo apt-key add -
+
+# Enable vscode repo
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+# Install vscode
+sudo apt update
+sudo apt install code
+
+echo ------------------
+echo Installing spotify
+echo ------------------
+echo
+
+# Adding repo and key
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 4773BD5E130D1D45
+sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
+
+# Install spotify-client
+sudo apt update
+sudo apt install spotify-client
+
+echo -------------------
+echo Installing zealdocs
+echo -------------------
+echo
+
+sudo apt-get install zeal
+
+echo -------------------
+echo Installing insomnia
+echo -------------------
+echo
+
+# Add to sources
+echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
+  | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+
+# Add public key used to verify code signature
+wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
+  | sudo apt-key add -
+
+# Refresh repository sources and install Insomnia
+sudo apt-get update
+sudo apt-get install insomnia
