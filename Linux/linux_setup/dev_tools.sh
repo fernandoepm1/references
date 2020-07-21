1761abca826976e8b684879293a9efb177e44c38
Fernando <fernando.epm1@gmail.com>
2020-07-21 19:26:10 -0300

    Add solargraph installation in dev_tools

--- Linux/linux_setup/dev_tools.sh
+++ Linux/linux_setup/dev_tools.sh
@@ -38,7 +38,10 @@ asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 
 asdf install ruby 2.7.1 # latest
 asdf global ruby 2.7.1
+
+# Install some essential gems
 gem install bundler
+gem install solargraph
 
 echo ---------------
 echo Installing node
