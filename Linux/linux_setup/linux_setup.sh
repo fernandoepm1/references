49166f725d2b1dffb6981bbb4f1615dcec21dd74
Fernando <fernando.epm1@gmail.com>
2020-07-21 18:52:25 -0300

    Update main setup to call other scripts

---
+++ Linux/linux_setup/linux_setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Essentials
+# git, curl and wget
+sh essentials.sh
+
+# Terminal Tweaks
+# zsh and oh-my-zsh
+sh terminal_tweaks.sh
+
+# Databases
+# postgres, pgAdmin4 and redis
+sh databases.sh
+
+# Dev Tools
+# asdf, ruby, nodejs and yarn
+sh dev_tools.sh
+
+# Software
+# chrome, vscode, spotify, zeal, insomnia 
+sh software.sh
+
+
+# TODO
+
+# install fonts
+sudo apt-get install fonts-powerline
+
+# install .ttf fonts
