194f8e1ff811f1f7dc98ef9c955dbb8da7efa4f1
Fernando <fernando.epm1@gmail.com>
2020-07-21 18:45:42 -0300

    Separate zsh installations in it's own file

---
+++ Linux/terminal_tweaks.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+echo --------------
+echo Installing zsh
+echo --------------
+echo
+
+sudo apt update
+sudo apt install zsh
+
+echo ------------------------
+echo Making zsh default shell
+echo ------------------------
+echo
+
+chsh -s $(which zsh)
+
+echo ---------------------
+echo Installing oh-my-zsh
+echo ---------------------
+echo
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo ---------------------------
+echo Installing oh-my-zsh themes
+echo ---------------------------
+echo
+
+# Powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel9k
+
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Change settings
+# code ~/.p10k.zsh
+# p10k configure
+
+echo ----------------------------
+echo Installing oh-my-zsh plugins
+echo ----------------------------
+echo
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# In WSL is giving permission issues,
+# so add this line before source ~/.oh-my-zsh.sh
+# ZSH_DISABLE_COMPFIX=true
+
+echo -------------------------------------
+echo Add oh-my-zsh plugins/theme to .zshrc
+echo -------------------------------------
+echo
+
+nano ~/.zshrc
