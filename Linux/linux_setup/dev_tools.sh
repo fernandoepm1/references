ee21824bc11ae48ee77c31263dae6a4a9d159123
Fernando <fernando.epm1@gmail.com>
2025-06-19 21:17:44 -0300

    Update asdf plugins installations

--- Linux/linux_setup/dev_tools.sh
+++ Linux/linux_setup/dev_tools.sh
@@ -1,19 +1,44 @@
 #!/usr/bin/env bash
 
+echo -------------
+echo Installing go
+echo -------------
+echo
+
+# Download linux-amd64 package
+# From https://go.dev/doc/install
+cd ~/ && wget https://go.dev/dl/go1.24.4.linux-amd64.tar.gz
+
+# Remove any previous installation
+sudo rm -rf /usr/local/go
+
+# Extract the downloaded archive and install it to /usr/local
+tar -C /usr/local -xzf go1.24.4.linux-amd64.tar.gz
+
+# Add go to PATH
+echo -e '\nexport PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
+
+# Verify installation
+go version
+
 echo ---------------
 echo Installing asdf
 echo ---------------
 echo
 
-# Docs https://asdf-vm.com/#/core-manage-asdf-vm
-# From https://github.com/asdf-vm/asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
+# Docs https://asdf-vm.com/guide/getting-started.html
 
-# Adding to zsh/bash
-echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
-echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
-# echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
-# echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+# Installing with Go
+# Default Go installation path is $HOME/go
+go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+
+# Set GOPATH and add asdf to PATH
+cd ~ && mv go ~/.go
+echo -e '\nGOPATH=$HOME/.go' >> ~/.zshrc
+echo -e '\nexport PATH=$GOPATH/bin' >> ~/.zshrc
+
+# Export asdf shims to PATH
+echo -e 'export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"' >> ~/.zshrc
 
 # Updating changes
 source ~/.zshrc
@@ -24,9 +49,12 @@ echo ----------------------------
 echo
 
 # From https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
-sudo apt-get install autoconf bison build-essential \
-  libssl-dev libyaml-dev libreadline6-dev zlib1g-dev \
-  libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
+# Required for the latest stable builds
+sudo apt-get install build-essential autoconf libssl-dev \
+  libyaml-dev zlib1g-dev libffi-dev libgmp-dev rustc
+
+# Required for old stable builds
+sudo apt-get install patch libreadline6-dev libncurses5-dev libgdbm6 libgdbm-dev libdb-dev
 
 echo ---------------
 echo Installing ruby
@@ -34,14 +62,18 @@ echo ---------------
 echo
 
 # From https://github.com/asdf-vm/asdf-ruby
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 
-asdf install ruby 2.7.1 # latest
-asdf global ruby 2.7.1
+# Create default gems file
+# This file is used to install gems automatically when a new Ruby version is installed
+touch ~/.default-gems
+echo -e "bundler" >> ~/.default-gems
+echo -e "pry" >> ~/.default-gems
+echo -e "solargraph" >> ~/.default-gems
 
-# Install some essential gems
-gem install bundler
-gem install solargraph
+# Install the latest stable Ruby version
+asdf install ruby latest
+
 
 echo ---------------
 echo Installing node
@@ -49,25 +81,42 @@ echo ---------------
 echo
 
 # From https://github.com/asdf-vm/asdf-nodejs
-asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
-asdf install nodejs 12.18.2 # LTS
-asdf global nodejs 12.18.2
+# Create default npm packages file
+# This file is used to install gems automatically when a new Ruby version is installed
+touch ~/.default-npm-packages
+echo -e "yarn" >> ~/.default-npm-packages
+echo -e "express" >> ~/.default-npm-packages
+echo -e "request" >> ~/.default-npm-packages
+echo -e "lodash" >> ~/.default-npm-packages
 
-echo ---------------
-echo Installing yarn
-echo ---------------
+asdf install nodejs latest
+
+echo -----------------
+echo Installing python
+echo -----------------
 echo
 
-# From https://classic.yarnpkg.com/en/docs/install/#debian-stable
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg \
-  | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" \
-  | sudo tee /etc/apt/sources.list.d/yarn.list
+# Add the plugin for Python
+asdf plugin add python https://github.com/asdf-community/asdf-python.git
 
+# Dependencies for Python
 sudo apt update
-sudo apt install yarn
+sudo apt install make build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev curl git \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+# Create default python packages file
+# This file is used to install packages automatically when a new Python version is installed
+touch ~/.default-python-packages
+echo -e "numpy" >> ~/.default-python-packages
+echo -e "pandas" >> ~/.default-python-packages
+echo -e "requests" >> ~/.default-python-packages
+
+# Install the latest stable Ruby version
+asdf install python latest
+
 
 echo ------------------------------
 echo Installing docker dependencies
